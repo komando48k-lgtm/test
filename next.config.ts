f35be31c25ccaf5215e010aa0fe8f66d89f60232
komando48k-lgtm <komando48k@gmail.com>
2026-02-17 17:29:21 +0200

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  trailingSlash: true,
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
+};
+
+export default nextConfig;
